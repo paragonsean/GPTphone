96f8e5bacf96fc1d49dd5823d1aac305039a6b46
Sean Baker <97693794+paragonsean@users.noreply.github.com>
2024-08-06 17:04:17 -0400

    working

---
+++ services/event_manager.py
@@ -0,0 +1,40 @@
+import asyncio
+from typing import Any, Callable, Dict, List
+
+'''
+Author: Sean Baker
+Date: 2024-07-22
+Description: STANDARD EVENT HANDLER
+'''
+class EventHandler:
+    """
+    A class that represents an event emitter.
+
+    An event emitter allows registering callbacks for specific events and emitting those events
+    with optional arguments and keyword arguments.
+    """
+
+    def __init__(self):
+        """
+        Initializes an instance of the EventEmitter class.
+        """
+        self._events: Dict[str, List[Callable]] = {}
+
+    def on(self, event: str, callback: Callable):
+
+        if event not in self._events:
+            self._events[event] = []
+        self._events[event].append(callback)
+
+    async def createEvent(self, event: str, *args: Any, **kwargs: Any):
+
+        if event in self._events:
+            for callback in self._events[event]:
+                await self._run_callback(callback, *args, **kwargs)
+
+    async def _run_callback(self, callback: Callable, *args: Any, **kwargs: Any):
+
+        if asyncio.iscoroutinefunction(callback):
+            await callback(*args, **kwargs)
+        else:
+            callback(*args, **kwargs)
